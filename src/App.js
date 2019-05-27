37f002d5e3aa8f25be1a69ac99aca43695ace78e
Your Name <you@example.com>
2019-05-26 22:14:00 -0700

    commit for weather

--- src/App.js
+++ src/App.js
@@ -4,57 +4,59 @@ import { async } from 'q';
 var apikey="5c8f256338798cfb853d99cac52df526";
 
 class App extends Component {
+
+  constructor()
+  {
+    super()
   
-  state={
+  this.state={
     temp:"",
     humidity:"",
     image:"",
     city:"",
     country:""
   }
+  this.handleClick=this.handleClick.bind(this)
+  this.handleChange=this.handleChange.bind(this)
+  this.handleChange1=this.handleChange1.bind(this)
+}
 
-  handleChange(value) {
+  handleChange(event) {
     this.setState({
-        city: value        
+        city:event.target.value     
     });
 }
-handleChange1(value) {
+handleChange1(event) {
   this.setState({      
-      country:value
+      country:event.target.value
   });
 }
 
  
 handleClick=async(e)=>{ 
-  const city=this.state.city
-  const country=this.state.country
   
   e.preventDefault();
-  const api=await fetch("http://api.openweathermap.org/data/2.5/weather?q=city,country&APPID=5c8f256338798cfb853d99cac52df526");
-  const rtn=await api.json();
-  console.log(rtn);
-
-
-    this.setState({
+    const APPID = "5c8f256338798cfb853d99cac52df526";
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.city},${this.state.country}&APPID=${APPID}`;
+    const api=await fetch(url)
+    const rtn=await api.json();
+   this.setState({
     temp:rtn.main.temp,
     humidity:rtn.main.humidity
-
-  });
-}
-
+   });
   
+  console.log(rtn);   
 
-
-
+}
   render() {
     return (
       <div className="container" id="root">
       <center>
         <div className="card" id="card1">
         
-<input type="text" placeholder="enter city" name="city" className="usercontrol" onChange={(event) =>this.handleChange(event.target.value)}/><br></br>
-<input type="text" placeholder="enter country" name="city" className="usercontrol" onChange={(event) =>this.handleChange1(event.target.value)}/><br></br>
-<button  className="btninfo" onClick={this.handleClick}>Get Wheather</button><br></br>
+        <input type="text" value={this.state.city} onChange={this.handleChange} required/><br></br>
+        <input type="text" value={this.state.country} onChange={this.handleChange1} required /><br></br>
+<button  className="btninfo" value="submit" onClick={this.handleClick}>Get Wheather</button><br></br>
 
         
         <h1>Temperature==</h1><h1>{this.state.temp}</h1><br></br>
